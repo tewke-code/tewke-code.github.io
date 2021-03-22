23534c108ac5b1061ace376b00aa4e2d2bb483e7
matt <matt@tewke.com>
2021-03-22 18:11:19 +0000

    Ref: Created screen management wrapper for multiple providers, tidied up logic, need to finish refactoring

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "7e35a68ca231d46db2bb9b491221182b",
-"main.dart.js": "d9d5b6352e35df4da1c10dcff0cd5c6c",
-"index.html": "c783355d1d1f4ebaf736a592853be465",
-"/": "c783355d1d1f4ebaf736a592853be465",
+"main.dart.js": "a40a6a0d69dc45b9b4e55ff5f0ed4f48",
+"index.html": "8e00032a03d561de8606237ae5c9f519",
+"/": "8e00032a03d561de8606237ae5c9f519",
 "assets/assets/icons/music/speaker_devices.svg": "69625faf508d3db05bc93a8a6530e84b",
 "assets/assets/icons/music/heart_favourite.svg": "37bcda30270cf0a8dc849a8b603d4dcf",
 "assets/assets/icons/music/heart_favourite_filled.svg": "fecf067e8e12533340d079f31e348279",
