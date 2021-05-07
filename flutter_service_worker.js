ccc785002cd7fe64653e1d6a79053c4c75ec6b1c
matt <matt@tewke.com>
2021-05-07 17:42:03 +0100

    Add: Completed button 2, all three buttons completed, need to polish on Monday

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "7e35a68ca231d46db2bb9b491221182b",
-"main.dart.js": "cb389c080b88fbad37eb634d98c58625",
-"index.html": "3da2bc1df9f98318a3cbab03c3e60608",
-"/": "3da2bc1df9f98318a3cbab03c3e60608",
+"main.dart.js": "d3615139ccd47ff504adcae2163bedc2",
+"index.html": "e016d98f4843c99177a399a53a4b0679",
+"/": "e016d98f4843c99177a399a53a4b0679",
 "assets/assets/icons/music/speaker_devices.svg": "69625faf508d3db05bc93a8a6530e84b",
 "assets/assets/icons/music/music-devices/tewke_switch_device.svg": "8cdb22062f8d12c9146cb9c32a617da4",
 "assets/assets/icons/music/music-devices/laptop_device.svg": "964edd6334b29d304e6b2df1a8d0aa18",
